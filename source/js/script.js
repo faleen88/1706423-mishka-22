2f4726085df7e501a66b4a120c6082e25b262c5c
Elena <enafukin@gmail.com>
2021-04-29 01:24:02 +0400

    Добавляет сборку проекта

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,20 @@
+const navMain = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toggle');
 const orderLink = document.querySelector('.button-order');
 const modalPopup = document.querySelector('.modal');
 const closePopup = document.querySelector('.form-choice__button');
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.remove('main-nav--opened');
+    navMain.classList.add('main-nav--closed');
+  }
+});
 
 orderLink.addEventListener('click', function (evt) {
   evt.preventDefault();
@@ -20,5 +34,4 @@ window.addEventListener("keydown", function (evt) {
     }
   }
 });
-
 
